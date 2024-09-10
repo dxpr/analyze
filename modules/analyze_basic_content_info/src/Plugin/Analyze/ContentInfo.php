c77d284b95eae9163c597a863434651f75c3ca7d
Paul Dale Smith <paul.smith@freelygive.org.uk>
2024-09-10 15:28:37 +0100

    Disable full report on the basic info plugin.

--- modules/analyze_basic_content_info/src/Plugin/Analyze/ContentInfo.php
+++ modules/analyze_basic_content_info/src/Plugin/Analyze/ContentInfo.php
@@ -6,6 +6,7 @@ namespace Drupal\analyze_basic_content_info\Plugin\Analyze;
 
 use Drupal\analyze\AnalyzePluginBase;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Url;
 
 /**
  * Analyze plugin to display Basic data.
@@ -39,59 +40,8 @@ final class ContentInfo extends AnalyzePluginBase {
   /**
    * {@inheritdoc}
    */
-  public function renderFullReport(EntityInterface $entity): array {
-    return [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Basic Content'),
-      'table' => [
-        '#type' => 'table',
-        '#header' => [['data' => 'Security', 'colspan' => 2, 'class' => ['header']]],
-        '#rows' => [
-          ['data' => ['Word count', 498]],
-          ['data' => ['Image count', 2]],
-        ],
-        '#attributes' => [
-          'class' => ['basic-data-table'],
-          'style' => ['table-layout: fixed;'],
-        ],
-      ],
-      'gauge_one' => [
-        '#theme' => 'analyze_gauge',
-        '#caption' => 'Sentiment Score - General Sentiment',
-        '#range_min_label' => 'Negative',
-        '#range_mid_label' => 'Neutral',
-        '#range_max_label' => 'Positive',
-        '#range_min' => 0,
-        // Example: 20% Positive.
-        '#value' => 0.2,
-        '#display_value' => '20%',
-        '#range_max' => 1,
-      ],
-      'gauge_two' => [
-        '#theme' => 'analyze_gauge',
-        '#caption' => 'Sentiment Score - Joy',
-        '#range_min_label' => 'Sad',
-        '#range_mid_label' => 'Neutral',
-        '#range_max_label' => 'Joyful',
-        '#range_min' => 0,
-        // Example: 60% Joyful.
-        '#value' => 0.6,
-        '#display_value' => '60%',
-        '#range_max' => 1,
-      ],
-      'gauge_three' => [
-        '#theme' => 'analyze_gauge',
-        '#caption' => 'Sentiment Score - Anger',
-        '#range_min_label' => 'Calm',
-        '#range_mid_label' => 'Neutral',
-        '#range_max_label' => 'Angry',
-        '#range_min' => 0,
-        // Example: 80% Angry.
-        '#value' => 0.8,
-        '#display_value' => '80%',
-        '#range_max' => 1,
-      ],
-    ];
+  public function getFullReportUrl(EntityInterface $entity): ?Url {
+    return NULL;
   }
 
 }
