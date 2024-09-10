42df468dd6c4bfc1ca3026966d602f2bcc2ebe38
Paul Dale Smith <paul.smith@freelygive.org.uk>
2024-09-10 16:35:28 +0100

    Update the basic info plugin to count words and images.

--- modules/analyze_basic_content_info/src/Plugin/Analyze/ContentInfo.php
+++ modules/analyze_basic_content_info/src/Plugin/Analyze/ContentInfo.php
@@ -60,14 +60,12 @@ final class ContentInfo extends AnalyzePluginBase implements ContainerFactoryPlu
    * {@inheritdoc}
    */
   public function renderSummary(EntityInterface $entity): array {
-    $word_count = $this->getWordCount($entity);
-
     return [
       '#type' => 'table',
-      '#header' => [['data' => 'Security', 'colspan' => 2, 'class' => ['header']]],
+      '#header' => [['data' => 'Basic Info', 'colspan' => 2, 'class' => ['header']]],
       '#rows' => [
-        ['data' => ['Word count', 498]],
-        ['data' => ['Image count', 2]],
+        ['data' => ['Word count', $this->getWordCount($entity)]],
+        ['data' => ['Image count', $this->getImageCount($entity)]],
       ],
       '#attributes' => [
         'class' => ['basic-data-table'],
@@ -87,24 +85,97 @@ final class ContentInfo extends AnalyzePluginBase implements ContainerFactoryPlu
    */
   private function getWordCount(EntityInterface $entity): int {
     $return = 0;
-    $countable = [];
-
-    // Handle potential differences in how titles are collected.
-    if (method_exists($entity, 'getTitle')) {
-      $countable[] = $entity->getTitle();
-    }
-    elseif (method_exists($entity, 'label')) {
-      $countable[] = $entity->label();
-    }
+    $countables = [];
 
     if ($entity instanceof FieldableEntityInterface) {
-      foreach ($this->entityFieldManager->getFieldDefinitions($entity->getEntityTypeId(), $entity->bundle()) as $definition) {
+      foreach ($this->getFieldDefinitionsByType($entity, [
+        'string',
+        'string_long',
+        'text_long',
+        'text_with_summary',
+        'text',
+      ]) as $definition) {
+        if ($name = $definition->getName()) {
+          if ($name !== 'timezone') {
+            $value = $entity->get($name)->getValue();
 
+            if (!empty($value[0]['value'])) {
+              $countables[] = $value[0]['value'];
+            }
+          }
+        }
       }
+    }
+    else {
 
+      // Handle potential differences in how titles are collected.
+      if (method_exists($entity, 'getTitle')) {
+        $countables[] = $entity->getTitle();
+      }
+      elseif (method_exists($entity, 'label')) {
+        $countables[] = $entity->label();
+      }
     }
 
+    foreach ($countables as $countable) {
+      $matches = [];
+      preg_match_all("/(\w+)/", strip_tags($countable), $matches);
 
+      if ($matches[0]) {
+        $return += count($matches[0]);
+      }
+    }
+
+    return $return;
+  }
+
+  /**
+   * Helper to get a count of images attached to an entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to count images on.
+   *
+   * @return int
+   *   The number of images. Defaults to 0.
+   */
+  private function getImageCount(EntityInterface $entity): int {
+    $return = 0;
+
+    if ($entity instanceof FieldableEntityInterface) {
+      foreach ($this->getFieldDefinitionsByType($entity, ['image']) as $definition) {
+        if ($name = $definition->getName()) {
+          if (!$entity->get($name)->isEmpty()) {
+            $values = $entity->get($name)->getValue();
+
+            $return += count($values);
+          }
+        }
+
+      }
+    }
+
+    return $return;
+  }
+
+  /**
+   * Helper to return field definitions of a given type.
+   *
+   * @param \Drupal\Core\Entity\FieldableEntityInterface $entity
+   *   The entity to get the definitions for.
+   * @param array $types
+   *   An array of types we want to return.
+   *
+   * @return \Drupal\Core\Field\FieldDefinitionInterface[]
+   *   Any definitions of the required types.
+   */
+  private function getFieldDefinitionsByType(FieldableEntityInterface $entity, array $types): array {
+    $return = [];
+
+    foreach ($this->entityFieldManager->getFieldDefinitions($entity->getEntityTypeId(), $entity->bundle()) as $definition) {
+      if (in_array($definition->getType(), $types)) {
+        $return[] = $definition;
+      }
+    }
 
     return $return;
   }
